8d9d8854bf32396efc9a63e39f0687708ffd52b6
unknown <cogjava3115@iiht.tech>
2022-08-18 12:57:06 +0530

    Added microservice functinalities

--- src/main/java/com/emp/controller/EmployeeController.java
+++ src/main/java/com/emp/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.emp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.emp.entity.Employee;
 import com.emp.service.IEmployeeService;
@@ -15,12 +18,19 @@ public class EmployeeController {
 
 	@Autowired
 	private IEmployeeService employeeService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 
 	@GetMapping("/{eId}")
 	public Employee getEmployee(@PathVariable("eId") long eId) {
-
 		Employee employee = employeeService.getEmployee(eId);
 
+		//http://localhost:8083/record/emp/1313
+		
+		List records = this.restTemplate.getForObject("http://localhost:8083/record/emp/"+ eId, List.class);
+		employee.setRecords(records);
 		return employee;
 	}
 }
